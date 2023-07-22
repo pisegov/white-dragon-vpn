cd50bfae013d6df681255b262bf0b364fdf178e7
Mikhail Pisegov <pisegov.mikhail@gmail.com>
2023-07-22 18:51:48 +0300

    add settings fragment

---
+++ app/src/main/java/com/example/whitedragonvpn/ui/settings_fragment/SettingsFragment.kt
@@ -0,0 +1,29 @@
+package com.example.whitedragonvpn.ui.settings_fragment
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.whitedragonvpn.databinding.FragmentSettingsBinding
+
+class SettingsFragment : Fragment() {
+
+    private var _binding: FragmentSettingsBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentSettingsBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+}
